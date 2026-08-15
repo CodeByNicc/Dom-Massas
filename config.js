21c2c0fb2671b58d0bed1a106ac3f3ad18e8350e
Nicole Oliveira <codebynic@gmail.com>
2026-08-15 17:16:57 -0300

    Horário normal, fim do teste

--- config.js
+++ config.js
@@ -4,14 +4,10 @@ const CONFIG = {
   orderCounterEndpoint: "https://script.google.com/macros/s/AKfycbxsmKCavhXvnAkWPxY1upPqHL92tK7GgvtBcaJTZYcHbIkAdBtXwY3V0AQuPhxSQaLD/exec",
   painelPin: "1234",
   businessHours: {
-    // closedWeekday: 1,
-    // ranges: [
-    //   { start: "11:00", end: "15:00" },
-    //   { start: "18:00", end: "22:00" }
-    // ]
-    closedWeekday: 7, // 7 não existe como dia da semana, então nunca fecha
+    closedWeekday: 1,
     ranges: [
-      { start: "00:00", end: "23:59" }
+    { start: "11:00", end: "15:00" },
+    { start: "18:00", end: "22:00" }
     ]
   }
 };
